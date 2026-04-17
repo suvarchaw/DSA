60d4c9e9165c1fec72b8b16549280dfd12dca043
Suvarcha Wadhwa <99994522+suvarchaw@users.noreply.github.com>
2026-04-17 11:43:00 +0530

    Time: 12 ms (58.84%) | Memory: 47.1 MB (75.90%) - LeetSync

---
+++ 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> findSubstring(String s, String[] words) {
+        List<Integer> result = new ArrayList<>();
+        if (s == null || s.length() == 0 || words.length == 0) return result;
+
+        int wordLen = words[0].length();
+        int wordCount = words.length;
+        int totalLen = wordLen * wordCount;
+
+        Map<String, Integer> wordMap = new HashMap<>();
+        for (String word : words) {
+            wordMap.put(word, wordMap.getOrDefault(word, 0) + 1);
+        }
+
+        for (int i = 0; i < wordLen; i++) {
+            int left = i, count = 0;
+            Map<String, Integer> windowMap = new HashMap<>();
+
+            for (int j = i; j + wordLen <= s.length(); j += wordLen) {
+                String word = s.substring(j, j + wordLen);
+
+                if (wordMap.containsKey(word)) {
+                    windowMap.put(word, windowMap.getOrDefault(word, 0) + 1);
+                    count++;
+
+                    while (windowMap.get(word) > wordMap.get(word)) {
+                        String leftWord = s.substring(left, left + wordLen);
+                        windowMap.put(leftWord, windowMap.get(leftWord) - 1);
+                        left += wordLen;
+                        count--;
+                    }
+
+                    if (count == wordCount) {
+                        result.add(left);
+                    }
+                } else {
+                    windowMap.clear();
+                    count = 0;
+                    left = j + wordLen;
+                }
+            }
+        }
+
+        return result;
+    }
+}
