f03ee681b15a973dfd9f9e42cf501390988b290f
Suvarcha Wadhwa <99994522+suvarchaw@users.noreply.github.com>
2026-07-24 10:21:07 +0530

    Time: 4 ms (3.63%) | Memory: 12.4 MB (20.26%) - LeetSync

---
+++ 198-house-robber/house-robber.py
@@ -0,0 +1,7 @@
+class Solution(object):
+       def rob(self, nums):
+          temp = [0, 0]
+          for n in nums:
+             temp[0], temp[1] = max(temp), n + temp[0]
+    
+          return max(temp)
