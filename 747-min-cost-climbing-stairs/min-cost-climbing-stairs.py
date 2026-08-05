46294a0d33d21ca04bf38e78dab2661ecd7ecd42
Suvarcha Wadhwa <99994522+suvarchaw@users.noreply.github.com>
2026-08-05 23:41:28 +0530

    Time: 2 ms (76.82%) | Memory: 12.4 MB (90.63%) - LeetSync

---
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -0,0 +1,6 @@
+class Solution(object):
+      def minCostClimbingStairs(self, cost):
+         temp = [0, cost[0]]
+         for i in range(1, len(cost)):
+             temp[0], temp[1] = temp[1], min(temp) + cost[i]
+         return min(temp)
